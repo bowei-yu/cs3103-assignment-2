4caf293d02b3952596436a3320c8aea49d11190d
junlong <65804451+junlong4321@users.noreply.github.com>
2021-11-15 22:31:57 +0800

    Add tests and algo
    33 % - Least response time (Non-weighted)
    33 % - Least response time (Positional Weighted)
    33% - Least connections (Positional Weighted)

--- jobScheduler.py
+++ jobScheduler.py
@@ -4,6 +4,10 @@ import sys
 import argparse
 import signal
 
+server_list = []
+initial_list = []
+server_request_times = {}
+
 # KeyboardInterrupt handler
 def sigint_handler(signal, frame):
     print('KeyboardInterrupt is caught. Close all sockets :)')
@@ -28,9 +32,53 @@ def getCompletedFilename(filename):
     # or track the number of concurrent files for each #
     # server?                                          #
     ####################################################
+    global server_list, server_request_times
+    server_name = server_request_times[filename][0]
+    request_time = server_request_times[filename][1]
 
-    # In this example. just print message
-    print(f"[JobScheduler] Filename {filename} is finished.")
+    # get response time
+    time_taken = (datetime.now() - request_time).total_seconds()
+
+    # find four_tuple in server list
+    four_tuple = None
+    position = None
+    for idx, val in enumerate(server_list):
+        if server_name in val:
+            four_tuple = val
+            position = idx
+            break
+
+    # Update N value
+    N = four_tuple[0]
+    active_connections = four_tuple[1]
+    response_time = four_tuple[2]
+    response_time = time_taken
+    active_connections = active_connections - 1
+
+    N = response_time * active_connections
+    weight = (10000 / ((len(server_list) - (idx + 1) + 1)))
+    weighted_response_time = N * weight
+    weighted_connection_time = active_connections * weight                
+
+    # Update four_tuple in server_list and sort
+    server_list[position][0] = (1/3 * N) + (1/3 * weighted_response_time) + (1/3 * weighted_connection_time)
+    server_list[position][1] = active_connections
+    server_list[position][2] = response_time
+    server_list.sort()
+
+    print('Completed')
+    print(server_list)
+
+    # # Adjust connection weight (cW) = Number of Active Connections x (10000 / Set Weight (sW))
+    # # Adjust time weight (cW) = Response Time x (10000 / Set Weight (sW))
+    # # Blend predictions by averaging out N value, connection_weight, and time weight
+    # for idx, val in enumerate(server_list):
+    #     if server_name in val:
+    #         weighted_response_time = N * (10000 / ((len(server_list) - (idx + 1) + 1)))
+    #         server_list[idx][0] = (0.5 * N) + (0.5 * weighted_response_time)
+    #         break
+    # server_list.sort()
+
 
 # formatting: to assign server to the request
 def scheduleJobToServer(servername, request):
@@ -44,14 +92,51 @@ def assignServerToRequest(servernames, request):
     # to assign this request? You can make decision.   #
     # You can use a global variables or add more       #
     # arguments.                                       #
-
+    ####################################################
     request_name = request.split(",")[0]
     request_size = request.split(",")[1]
 
-    # Example. just assign the first server
-    server_to_send = servernames[0]
+    # # Example. just assign the first server
+    # server_to_send = servernames[0]
 
-    ####################################################
+    # Get variables
+    global server_list, server_request_times, start_list
+
+    # If servers are not initialized, initialize using initial list first (ie push out jobs and spread it across to all servers)
+    if initial_list:
+        four_tuple = initial_list.pop(0)
+    else:
+        four_tuple = server_list[0]
+
+    N = four_tuple[0]
+    active_connections = four_tuple[1]
+    response_time = four_tuple[2]
+    server_name = four_tuple[3]
+
+    # Increment active connections
+    server_to_send = server_name
+    active_connections = active_connections + 1        
+
+    for idx, val in enumerate(server_list):
+        if server_name in val:
+            server_list[idx][1] = active_connections
+            # Prevents bug where N = 0 and only 1 server keeps getting assigned jobs
+            if response_time != 0:
+                N = response_time * active_connections
+                weight = (10000 / ((len(server_list) - (idx + 1) + 1)))
+                weighted_response_time = N * weight
+                weighted_connection_time = active_connections * weight
+                server_list[idx][0] = (1/3 * N) + (1/3 * weighted_response_time) + (1/3 * weighted_connection_time)
+            break
+    server_list.sort()
+
+    print('Initial list')
+    print(initial_list)
+    print('Sent')
+    print(server_list)
+
+    # Record time of request to server
+    server_request_times[request_name] = [server_name, datetime.now()]
 
     # Schedule the job
     scheduled_request = scheduleJobToServer(server_to_send, request)
@@ -103,6 +188,16 @@ if __name__ == "__main__":
     binaryServernames = serverSocket.recv(4096)
     servernames = parseServernames(binaryServernames)
     print(f"Servernames: {servernames}")
+
+    for i in servernames:
+        # (response time * number of active connections, servername, response time, number of active connections)
+        response_time = 0
+        active_connections = 0
+        N = 100000
+        server_list.append([N, active_connections, response_time, i])
+    print(server_list)
+    # For initializing all servers
+    initial_list = server_list.copy()
 
     currSeconds = -1
     now = datetime.now()
